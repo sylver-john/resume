0c93d29ca6b9f377201667becfe26259ad63be57
sylver-john <sylverimhoff@gmail.com>
2020-12-03 14:15:46 +0100

    feat: add build

---
+++ _nuxt/ad92b82.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{155:function(n,t,e){"use strict";var c=e(24),component=Object(c.a)({},(function(){var n=this.$createElement,t=this._self._c||n;return t("div",[t("Nuxt")],1)}),[],!1,null,null,null);t.a=component.exports},158:function(n,t,e){e(159),n.exports=e(160)}},[[158,3,1,4]]]);
